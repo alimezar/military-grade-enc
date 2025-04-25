d0976038cd9eb51f1ddadfc4eb7ca1067ac495df
Ali Sherif <ali.mezar@gmail.com>
2025-04-25 22:32:01 +0300

    Challenge Update

--- Dockerfile
+++ Dockerfile
@@ -2,8 +2,6 @@ FROM php:8.1-apache
 
 COPY web/ /var/www/html
 
-COPY flag1.txt /flag1.txt
-
-RUN chown -R www-data:www-data /var/www/html /flag1.txt
+RUN chown -R www-data:www-data /var/www/html
 
 EXPOSE 80
--- web/config.php
+++ web/config.php
@@ -1,3 +1,12 @@
 <?php
-$ENC_SECRET_KEY = "GIUCTF{M1LITARYCryp70}";
+// -------------------------------
+// Military-grade super-secret key
+// -------------------------------
+$ENC_KEY = 'HWeAjEzKgWAAPabaHaYMuKLr';
+
+// -------------------------------
+// Flags
+// -------------------------------
+$FLAG_PHASE1 = 'GIUCTF{login_as_admin_to_win}';
+$FLAG_PHASE2 = 'GIUCTF{1_byt3_brut3_f0rc3_ftw}';
 ?>
