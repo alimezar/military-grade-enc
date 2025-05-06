139cb6bf71dbdb6b93f0a2fed844f694d0118090
Ali Sherif <ali.mezar@gmail.com>
2025-05-06 12:16:35 +0300

    Small changes

--- web/config.php
+++ web/config.php
@@ -1,6 +1,6 @@
 <?php
 /* config.php */
 $ENC_KEY     = 'HWeAjEzKgWAAPabaHaYMuKLr';          // 24 bytes
-$FLAG_PHASE1 = 'GIUCTF{login_as_admin_to_win}';
+$FLAG_PHASE1 = 'GIUCTF{N0T_S0_S3CUR3_AFT3RALL}';
 $FLAG_PHASE2 = 'GIUCTF{1_byt3_brut3_f0rc3_ftw}';
 ?>
